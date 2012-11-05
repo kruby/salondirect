ed4d60a966403405d54148281de94386240322cb
Thomas Korsgaard Schmidt <ts@kruby.dk>
2012-11-05 19:38:57 +0100

    Rettelse :login til :name

--- db/migrate/014_add_default_user.rb
+++ db/migrate/014_add_default_user.rb
@@ -1,7 +1,7 @@
 class AddDefaultUser < ActiveRecord::Migration
   def self.up
     if !User.find_by_login('thomas')
-      User.create(:login => 'thomas', :email => 'ts@kruby.dk',
+      User.create(:name => 'thomas', :email => 'ts@kruby.dk',
       :password => 'kf1737', :password_confirmation => 'kf1737', :category => 'Admin', :blogname => 'TKS')
     end
   end
